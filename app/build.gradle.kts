76f2355a8ce0d0e316b66785283b088eb08810f5
Muhammad Lutfi Aziz <mynameis.lutfii@gmail.com>
2025-11-30 00:25:50 +0700

    feat(Firebase): Setup Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -41,6 +42,8 @@ android {
 }
 
 dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.animation)
