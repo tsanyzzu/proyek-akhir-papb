c2645b5917008b80ccea22e201572bf27396caf1
faizfznn <faiz150605@gmail.com>
2025-11-30 02:04:58 +0700

    add video

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation("androidx.media3:media3-exoplayer:1.2.0") // Or latest version
     implementation("androidx.media3:media3-ui:1.2.0")
     implementation("com.google.code.gson:gson:2.11.0")
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.animation)
     implementation(libs.androidx.material.icons.extended)
